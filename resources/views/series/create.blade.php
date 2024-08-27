6ebcb3d945efbdf42148e9d95c230441bc78d85c
jtonyNet <jtony.net@gmail.com>
2024-08-27 14:33:06 -0300

    Exibindo erros de validacoes de request customizada

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Nova Serie">
     <a href="{{ route('series.index') }}" class="btn btn-dark mb-2">Ver Series</a> </br>
 
-    <x-series.form :action="route('series.store')" />
+    <x-series.form :action="route('series.store')" :nome="old('nome')" />
 </x-layout>
